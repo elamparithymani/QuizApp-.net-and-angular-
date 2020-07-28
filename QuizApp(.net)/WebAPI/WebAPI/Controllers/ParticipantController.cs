b3ab08bcfa8a6d434c3e9e341f98a342d975a485
Elamparithy M <elamparithym@xpanxion.com>
2020-07-28 13:21:21 +0530

    .net and angular commit

---
+++ QuizApp(.net)/WebAPI/WebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ParticipantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+        public Participant Insert(Participant model)
+        {
+            using (QuizDBEntities db = new QuizDBEntities())
+            {
+                db.Participants.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+        }
+
+    
+
+    [HttpPost]
+        [Route("api/UpdateOutput")]
+        public void UpdateOutput(Participant model)
+        {
+            using (QuizDBEntities db = new QuizDBEntities())
+            {
+                db.Entry(model).State = System.Data.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
